44032afa323147b775f8ec6396b3e8b959dc74d7
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-01-12 17:58:34 -0300

    testimonios

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
-import React from "react"
+import React, { useRef, useState } from 'react';
 import { getTestimonials } from '../../redux/actions';
 import { useDispatch, useSelector} from "react-redux"
-import Carousel from "react-elastic-carousel";
-import Item from "./Item";
-import './styles.css'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css/navigation'; 
 import { useAuth0 } from "@auth0/auth0-react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import './styles.css';
+import { Navigation } from 'swiper/modules';
 
 
 
@@ -29,61 +29,68 @@ const Testimonials = () => {
 
     let {user, isAuthenticated} = useAuth0();
 
-	
+	console.log(testimonials)
 
 	
     return(
 
-<section className="conteiner">
-<div class="row">
-<div class="col-lg-6 mx-auto">
+        <section className="my-section">
 		
-			
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+            {testimonials && testimonials.map((e)=>{
+
+                return(
+
+
+
+                <SwiperSlide>
+
+
+                    
+                   <div className='mytestimonial'>
+
+                    <div className='testi-content'>
+
+                        <div className='slide'>
+
+
+                            <p>{e.description}</p>
+                            <i class='bx bxs-quote-alt-left quote-icon'></i>
+
+                            <div className='mydetails'>
+                                <span className='name'>Sofia</span>
+                            </div>
+
+
+
+
+                        </div>
+
+
+
+                    </div>
+
+
+                   </div>
+                
+                
+                </SwiperSlide>
+                
+                
+                )
+
+
+
+            })}
         
-        <Swiper
-
-spaceBetween={50}
-slidesPerView={3}
-
-
         
-        >
-
-      { testimonials && testimonials.map((e)=>{
-
-        return (
-
-          
-
-            <SwiperSlide>
-
-           
-                <div className='testimonial'>
-                  <blockquote className="blockquote blockquote-custom bg-white p-5 shadow rounded">
-                    <div className="blockquote-custom-icon bg-info shadow-sm"><i className="fa fa-quote-left text-white"></i></div>
-                    <p className="mb-0 mt-2 font-italic">{e.description} <a href="#" className="text-info">@consequat</a>."</p>
-                    <footer className="blockquote-footer pt-4 mt-4 border-top">Someone famous in
-                        <cite title="Source Title">Source Title</cite>
-                    </footer>
-                </blockquote>
-
-      </div>
-            </SwiperSlide>
-
-        )
-
-
-
-      })
-
-     
-      
-      }
-      
       </Swiper>
-</div>
-</div>
-</section>
+
+
+      </section>
+
+
         
     )
 }
