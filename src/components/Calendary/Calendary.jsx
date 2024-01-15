e5c5399110cb0c9eda263dcd3b56a89ca0405e3c
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-01-15 16:47:33 -0300

    pagos

--- src/components/Calendary/Calendary.jsx
+++ src/components/Calendary/Calendary.jsx
@@ -26,6 +26,8 @@ const Calendary = () => {
     <>
     <div className="App">
 
+    
+
 <div className="section-title st-center">
 						<h3>Congratulations! You have a session with Mental Flex</h3>
 						<p>Now you can schedule an appointment</p>
@@ -35,13 +37,15 @@ const Calendary = () => {
 
 
             <InlineWidget url="https://calendly.com/mentalflex2024" />
+            
       
       </div>
+      <a href="http://localhost:3001" className="btn btn-main btn-lg">Click to go back to Mental Flex</a> 
       </div>
 
       
     </div>
-    <a href="http://localhost:3001" className="btn btn-default btn-lg left-btn">Contact Us</a> 
+    
     
     </>
   );
