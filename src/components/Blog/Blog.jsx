9c7f95fee63a53144cbe59c10ddc452071f9ea9c
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2024-02-06 00:55:09 -0300

    blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,37 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { filterByCategory, getAllPublications, getCategories } from "../../redux/actions";
-import CardsContainer from "../Blog/CardsContainer";
-import NavBar from "../NavBar/NavBar";
 import { useTranslation } from 'react-i18next';
 import './Blog.css';
 
 const Blog = () => {
   const [t, i18n] = useTranslation("global");
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const [selectedFilter, setSelectedFilter] = useState('All');
-  const dispatch = useDispatch();
-  const publications = useSelector(state => state.publications);
-  const categories = useSelector(state => state.categories);
-  const filtered = useSelector(state => state.filtered);
+  
 
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log("Dispatching getCategories and getAllPublications");
-      await dispatch(getCategories());
-      await dispatch(getAllPublications());
-    };
-
-    fetchData();
-  }, []);
-
-  const filter = (event) => {
-    const value = event.target.value;
-    setSelectedFilter(value);
-    dispatch(filterByCategory(value));
-    setSelectedCategory(value);
-  };
-
+  
   return (
     <div data-spy="scroll" data-target=".main-nav">
       <section className="portfolio" id="portfolio">
@@ -43,25 +18,19 @@ const Blog = () => {
                 <p>{t("blog.blog2")}</p>
               </div>
 
-              <div className="filter mb40">
-                <form id="filter">
-                  <fieldset className="group" name="category">
-                    <label className={`btn btn-default ${selectedCategory === 'All' ? 'selected' : ''}`}>
-                      <input type="radio" name="filter" value="All" onChange={(e) => filter(e)} checked={selectedFilter === 'All'} />
-                      All
-                    </label>
-                    {categories?.map((c) => (
-                      <label key={c.name} className={`btn btn-default ${selectedCategory === c.name ? 'selected' : ''}`}>
-                        <input type="radio" name="filter" value={c.name} onChange={(e) => filter(e)} checked={selectedFilter === c.name} />
-                        {c.name}
-                      </label>
-                    ))}
-                  </fieldset>
-                </form>
-              </div>
+              <div style={{ marginLeft: '500px' }}>
+      <iframe
+        title="BlogFrame"
+        width="70%"
+        height="600"
+        src="https://mental-flex.blogspot.com/"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
 
-              {console.log('Publications:', publications)}
-              <CardsContainer publications={filtered.length === 0 ? publications : filtered} />
+           
+
             </div>
           </div>
         </div>
